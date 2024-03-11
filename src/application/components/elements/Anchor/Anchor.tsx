d61df676da0fff2d7799591713ffbfd6a43830e6
Reign-Beaux <samc920130@hotmail.com>
2024-03-10 19:11:32 -0600

    se agregaron componentes

---
+++ src/application/components/elements/Anchor/Anchor.tsx
@@ -0,0 +1,31 @@
+import { Link, LinkProps } from "react-router-dom";
+import "./styles.css";
+
+export interface AnchorProps extends LinkProps {
+  type?: "text" | "button";
+  isExternal?: boolean;
+}
+
+export const Anchor = ({
+  children,
+  type = "text",
+  className,
+  isExternal = false,
+  ...props
+}: AnchorProps) => {
+  const classes = type === "text" ? "anchor" : "anchor__button" + " " + className;
+
+  return (
+    <>
+      {isExternal ? (
+        <a className={classes} href={props.to.toString()} target="_blank" rel="noopener noreferrer">
+          {children}
+        </a>
+      ) : (
+        <Link className={classes} {...props}>
+          {children}
+        </Link>
+      )}
+    </>
+  );
+};
