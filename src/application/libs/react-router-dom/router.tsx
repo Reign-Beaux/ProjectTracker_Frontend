a8bb1923d043488c8d82353cd7349b48ac775d66
Reign-Beaux <samc920130@hotmail.com>
2024-03-10 18:47:42 -0600

    se agrego ruta

--- src/application/libs/react-router-dom/router.tsx
+++ src/application/libs/react-router-dom/router.tsx
@@ -1,11 +1,15 @@
 import { Login } from "features/login";
-import { RouteObject, createBrowserRouter } from "react-router-dom";
+import { Navigate, RouteObject, createBrowserRouter } from "react-router-dom";
 import { PagePaths } from ".";
 
 const routes: RouteObject[] = [
   {
     path: PagePaths.LOGIN,
     element: <Login />,
+  },
+  {
+    path: PagePaths.ERROR_PATH,
+    element: <Navigate to={PagePaths.LOGIN} />,
   },
 ];
 
