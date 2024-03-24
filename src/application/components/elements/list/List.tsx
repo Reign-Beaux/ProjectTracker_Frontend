03fe83081d34dd572917bdd7e312a1dfb1111e07
Reign-Beaux <samc920130@hotmail.com>
2024-03-24 14:09:57 -0600

    se creo store para apllication

--- src/application/components/elements/list/List.tsx
+++ src/application/components/elements/list/List.tsx
@@ -1,5 +1,6 @@
 import { Fragment, ReactNode, useState } from "react";
 import "./styles.css";
+import { Icon } from "..";
 
 interface ListProps {
   text: string;
@@ -9,9 +10,13 @@ interface ListProps {
 export const List = ({ text, items }: ListProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
+  const currentIcont = isOpen ? "ChevronUp" : "ChevronDown";
+
   return (
     <div className="list" onClick={() => setIsOpen(!isOpen)}>
-      <div className="list__text">{text}</div>
+      <div className="list__text">
+        {text} <Icon name={currentIcont} />
+      </div>
       {isOpen && (
         <div className="list__container">
           {items.map((item, index) => (
