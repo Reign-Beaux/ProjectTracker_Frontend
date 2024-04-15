9a54994e7dcbdc3136b27ea7885d69f140def1f5
Reign-Beaux <samc920130@hotmail.com>
2024-04-15 00:13:29 -0600

    mejoras

--- src/application/components/layouts/main-layout/components/header/header.tsx
+++ src/application/components/layouts/main-layout/components/header/header.tsx
@@ -1,29 +1,30 @@
-import { AppBar, Box, Button, IconButton, Toolbar, Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import { Icon } from "application/components/elements";
 import { useMainLayoutContext } from "../../context";
+import "./styles.css";
 
 export const Header = () => {
   const { setIsOpenSidenav } = useMainLayoutContext();
 
+  const projectName = "Project Tracker";
+
   return (
-    <Box component="header" sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={() => setIsOpenSidenav((prev) => !prev)}>
-            <Icon type="menu" />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
-          </Typography>
-          <Button color="inherit">Login</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <header className="main-layout__header">
+      <IconButton
+        size="large"
+        edge="start"
+        color="inherit"
+        aria-label="menu"
+        sx={{ mr: 2 }}
+        onClick={() => setIsOpenSidenav((prev) => !prev)}>
+        <Icon type="menu" />
+      </IconButton>
+      <Typography variant="h6" component="div" sx={{ userSelect: "none", flexGrow: 1 }}>
+        {projectName}
+      </Typography>
+      <Typography component="div" sx={{ userSelect: "none" }}>
+        Brenda Lizbeth Vazquez Tolentino
+      </Typography>
+    </header>
   );
 };
