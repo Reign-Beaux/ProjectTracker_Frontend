ccbdabe17a6ba228d1223ae2e3a1f3098a60c16f
Reign-Beaux <samc920130@hotmail.com>
2024-04-16 00:00:53 -0600

    se agrega el layout a otros componentes

--- src/features/system/roles/Roles.tsx
+++ src/features/system/roles/Roles.tsx
@@ -1,6 +1,9 @@
+import { MainLayout } from "application/components/layouts";
 
 export const Roles = () => {
   return (
+    <MainLayout title="Roles">
       <h1>Roles</h1>
+    </MainLayout>
   );
 };
--- src/features/system/users/Users.tsx
+++ src/features/system/users/Users.tsx
@@ -1,6 +1,9 @@
+import { MainLayout } from "application/components/layouts";
 
 export const Users = () => {
   return (
+    <MainLayout title="Users">
       <h1>Users Working!!!</h1>
-  )
-}
+    </MainLayout>
+  );
+};
