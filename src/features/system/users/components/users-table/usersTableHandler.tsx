5a964bd6bba7297378544510908c423814c169f7
Reign-Beaux <samc920130@hotmail.com>
2024-05-22 23:28:11 -0600

    correcciones

--- src/features/system/users/components/users-table/usersTableHandler.tsx
+++ src/features/system/users/components/users-table/usersTableHandler.tsx
@@ -141,8 +141,6 @@ export const useUsersTableHandler = () => {
 
   const columns = columnsSettings(updateRecord, deleteRecord);
 
-  useEffect(() => {}, []);
-
   useEffect(() => {
     getDataSource();
   }, [settingsTable.pagination, settingsTable.sort]);
