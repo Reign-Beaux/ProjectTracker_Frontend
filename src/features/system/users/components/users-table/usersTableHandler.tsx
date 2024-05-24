d475552dd11fd0511214bbeae6a6d79518fc49ad
Reign-Beaux <samc920130@hotmail.com>
2024-05-23 22:28:01 -0600

    corrección

--- src/features/system/users/components/users-table/usersTableHandler.tsx
+++ src/features/system/users/components/users-table/usersTableHandler.tsx
@@ -29,117 +29,113 @@ export const useUsersTableHandler = () => {
     setState({ settingsTable: { ...settingsTable, sort: newSort } } as StateProps);
   };
 
-  const updateRecord = () => {
-    console.log("updateRecord");
+  const updateRecord = (id: number) => {
+    console.log("updateRecord: " + id);
   };
 
-  const deleteRecord = () => {
-    console.log("deleteRecord");
+  const deleteRecord = (id: number) => {
+    console.log("deleteRecord: " + id);
   };
 
-  const columnsSettings = (update: Function, remove: Function): GridColDef[] => {
-    return [
-      {
-        field: "name",
-        flex: 1,
-        minWidth: 150,
-        editable: false,
-        renderHeader: () => (
-          <TableHeader
-            field="name"
-            text="Nombre(s)"
-            currentColmunSort={currentColmunSort}
-            setCurrentColmunSort={setCurrentColmunSort}
-            changeSort={changeSort}
-          />
-        ),
-      },
-      {
-        field: "paternalLastname",
-        flex: 1,
-        minWidth: 150,
-        editable: false,
-        renderHeader: () => (
-          <TableHeader
-            field="paternalLastname"
-            text="Apellido Paterno"
-            currentColmunSort={currentColmunSort}
-            setCurrentColmunSort={setCurrentColmunSort}
-            changeSort={changeSort}
-          />
-        ),
-      },
-      {
-        field: "maternalLastname",
-        flex: 1,
-        minWidth: 150,
-        editable: false,
-        renderHeader: () => (
-          <TableHeader
-            field="maternalLastname"
-            text="Apellido Materno"
-            currentColmunSort={currentColmunSort}
-            setCurrentColmunSort={setCurrentColmunSort}
-            changeSort={changeSort}
-          />
-        ),
-      },
-      {
-        field: "username",
-        flex: 1,
-        minWidth: 150,
-        editable: false,
-        renderHeader: () => (
-          <TableHeader
-            field="username"
-            text="Usuario"
-            currentColmunSort={currentColmunSort}
-            setCurrentColmunSort={setCurrentColmunSort}
-            changeSort={changeSort}
-          />
-        ),
-      },
-      {
-        field: "email",
-        flex: 1,
-        minWidth: 150,
-        editable: false,
-        renderHeader: () => (
-          <TableHeader
-            field="email"
-            text="Email"
-            currentColmunSort={currentColmunSort}
-            setCurrentColmunSort={setCurrentColmunSort}
-            changeSort={changeSort}
-          />
-        ),
-      },
-      {
-        field: "actions",
-        headerName: "",
-        flex: 1,
-        minWidth: 150,
-        editable: false,
-        align: "center",
-        renderCell: (params: GridRenderCellParams) => (
-          <>
-            <Tooltip title="Actualizar usuario">
-              <IconButton aria-label="update-student" onClick={() => update(params.row.id)}>
-                <Icon type="edit" />
-              </IconButton>
-            </Tooltip>
-            <Tooltip title="Eliminar usuario">
-              <IconButton aria-label="delete-subject" onClick={() => remove(params.row.id)}>
-                <Icon type="delete" />
-              </IconButton>
-            </Tooltip>
-          </>
-        ),
-      },
-    ];
-  };
-
-  const columns = columnsSettings(updateRecord, deleteRecord);
+  const columns: GridColDef[] = [
+    {
+      field: "name",
+      flex: 1,
+      minWidth: 150,
+      editable: false,
+      renderHeader: () => (
+        <TableHeader
+          field="name"
+          text="Nombre(s)"
+          currentColmunSort={currentColmunSort}
+          setCurrentColmunSort={setCurrentColmunSort}
+          changeSort={changeSort}
+        />
+      ),
+    },
+    {
+      field: "paternalLastname",
+      flex: 1,
+      minWidth: 150,
+      editable: false,
+      renderHeader: () => (
+        <TableHeader
+          field="paternalLastname"
+          text="Apellido Paterno"
+          currentColmunSort={currentColmunSort}
+          setCurrentColmunSort={setCurrentColmunSort}
+          changeSort={changeSort}
+        />
+      ),
+    },
+    {
+      field: "maternalLastname",
+      flex: 1,
+      minWidth: 150,
+      editable: false,
+      renderHeader: () => (
+        <TableHeader
+          field="maternalLastname"
+          text="Apellido Materno"
+          currentColmunSort={currentColmunSort}
+          setCurrentColmunSort={setCurrentColmunSort}
+          changeSort={changeSort}
+        />
+      ),
+    },
+    {
+      field: "username",
+      flex: 1,
+      minWidth: 150,
+      editable: false,
+      renderHeader: () => (
+        <TableHeader
+          field="username"
+          text="Usuario"
+          currentColmunSort={currentColmunSort}
+          setCurrentColmunSort={setCurrentColmunSort}
+          changeSort={changeSort}
+        />
+      ),
+    },
+    {
+      field: "email",
+      flex: 1,
+      minWidth: 150,
+      editable: false,
+      renderHeader: () => (
+        <TableHeader
+          field="email"
+          text="Email"
+          currentColmunSort={currentColmunSort}
+          setCurrentColmunSort={setCurrentColmunSort}
+          changeSort={changeSort}
+        />
+      ),
+    },
+    {
+      field: "actions",
+      headerName: "",
+      flex: 1,
+      minWidth: 150,
+      editable: false,
+      align: "center",
+      renderCell: (params: GridRenderCellParams) => (
+        <>
+          <Tooltip title="Actualizar usuario">
+            <IconButton aria-label="update-student" onClick={() => updateRecord(params.row.id)}>
+              <Icon type="edit" />
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Eliminar usuario">
+            <IconButton aria-label="delete-subject" onClick={() => deleteRecord(params.row.id)}>
+              <Icon type="delete" />
+            </IconButton>
+          </Tooltip>
+        </>
+      ),
+    },
+  ];
 
   useEffect(() => {
     getDataSource();
